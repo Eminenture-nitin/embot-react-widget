24c6b22d7f8c489d5957e21780461796e3863863
Nitin Kadam <nitinkadam.eminenture@gmail.com>
2024-06-17 15:42:46 +0530

    create image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,7 @@ const Header = () => {
   return (
     <div className="flex space-y-1.5 px-6 py-4 bg-gray-900">
       <div className="w-24 w-h-auto object-cover">
-        <img
-          src={require("../images/embotLogo.webp")}
-          className="w-full h-full"
-        />
+        <img src="/images/embotLogo.webp" className="w-full h-full" />
       </div>
     </div>
   );
