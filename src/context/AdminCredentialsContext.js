7508fc370ee37eebceb3e2b6ff869cb365f753e6
Nitin Kadam <nitinkadam.eminenture@gmail.com>
2024-07-04 10:21:21 +0530

    adding new changes

--- src/context/AdminCredentialsContext.js
+++ src/context/AdminCredentialsContext.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { createContext, useContext, useEffect, useState } from "react";
+import { customDehash } from "../utils/validations";
 
 const AdminCredentialsContext = createContext();
 
@@ -10,7 +11,8 @@ export function useAdminCredentials() {
 export const AdminCredentialsProvided = ({ children }) => {
   const [theme, setTheme] = useState("");
   const [adminEmail, setAdminEmail] = useState("");
-  const adminId = "650d432aa0570859518c23a1";
+  const [adminId, setAdminId] = useState(null);
+
   const getAdminData = () => {
     axios
       .get(
@@ -23,6 +25,42 @@ export const AdminCredentialsProvided = ({ children }) => {
       })
       .catch((e) => console.log(e));
   };
+
+  useEffect(() => {
+    // Function to check for EMChatBotAdminId
+    const checkForAdminId = () => {
+      if (window.EMChatBotData && window.EMChatBotData.EMChatBotAdminId) {
+        const dehashId = customDehash(
+          window.EMChatBotData.EMChatBotAdminId,
+          "EMReact"
+        );
+        setAdminId(dehashId);
+        return true;
+      }
+      return false;
+    };
+
+    // Initial check
+    if (!checkForAdminId()) {
+      // Polling mechanism
+      const intervalId = setInterval(() => {
+        if (checkForAdminId()) {
+          clearInterval(intervalId); // Stop checking once the value is found
+        }
+      }, 1000); // Check every second
+
+      // Optional: Set a timeout to stop checking after a certain period
+      const timeoutId = setTimeout(() => {
+        clearInterval(intervalId); // Stop checking after timeout
+      }, 10000); // Stop checking after 10 seconds
+
+      // Cleanup on component unmount
+      return () => {
+        clearInterval(intervalId);
+        clearTimeout(timeoutId);
+      };
+    }
+  }, []);
   useEffect(() => {
     getAdminData();
   }, []);
--- src/context/TriggersDataContext.js
+++ src/context/TriggersDataContext.js
@@ -49,7 +49,7 @@ export function TriggersContextProvider({ children }) {
         `${process.env.REACT_APP_API_URL}/auth/getNodeAndEdgesWidget/${adminId}`
       )
       .then((res) => {
-       // console.log(res.data);
+        // console.log(res.data);
         setNodes(res.data.nodes);
         setEdges(res.data.edges);
         startChatbot(res.data.nodes, res.data.edges);
@@ -209,14 +209,14 @@ export function TriggersContextProvider({ children }) {
       console.error(`Node with ID "${nextNodeId}" not found in nodes.`);
       return;
     }
-   // console.log(connectedNode);
+    // console.log(connectedNode);
     // Activate the connected node
     activateNode(connectedNode, nodes, edges, new Set());
   };
 
   // Function to handle "Questionable Trigger"
   const handleQuestionableTrigger = (node, nodes, edges) => {
-   // console.log(node.data.message.validationType);
+    // console.log(node.data.message.validationType);
     if (
       node.data.message.validationType == "Email" &&
       isValueInCookies("widget_user_email")
@@ -242,18 +242,18 @@ export function TriggersContextProvider({ children }) {
         nodeId: node.id,
       };
 
-     // console.log("questionableMessage", questionableMessage);
+      // console.log("questionableMessage", questionableMessage);
       setChatMessages((prevMessages) => [...prevMessages, questionableMessage]);
     }
   };
 
   const questionableTUserInteraction = (value) => {
     if (inputTagConfig.validationType == "Email" && isValidEmail(value)) {
-     // console.log("email is verify");
+      // console.log("email is verify");
       Cookies.set("widget_user_email", value, { expires: 3 });
       handleUserDecision(inputTagConfig.nextNodeId, value);
     } else if (inputTagConfig.validationType == "Name" && isValidName(value)) {
-     // console.log("Name is correct");
+      // console.log("Name is correct");
       handleUserDecision(inputTagConfig.nextNodeId, value);
     } else if (
       inputTagConfig.validationType == "Phone Number" &&
