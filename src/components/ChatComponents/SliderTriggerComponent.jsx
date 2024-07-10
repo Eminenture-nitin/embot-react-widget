44d91adae04adb6b1c7bdd72e7615b247d22daa5
Nitin Kadam <nitinkadam.eminenture@gmail.com>
2024-07-10 13:48:21 +0530

    made react app as a shadow dom

--- src/components/ChatComponents/SliderTriggerComponent.jsx
+++ src/components/ChatComponents/SliderTriggerComponent.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { Splide, SplideSlide } from "@splidejs/react-splide";
-import "@splidejs/react-splide/css";
 import { useTriggersContextData } from "../../context/TriggersDataContext";
 import Carousel from "./Carousel";
 
