07e1692e3224018dcb99c1a8096a59d21965725e
Nitin Kadam <nitinkadam.eminenture@gmail.com>
2024-08-06 10:52:36 +0530

    adding new ripple effect in icon

--- src/components/BOTParentComponent.jsx
+++ src/components/BOTParentComponent.jsx
@@ -11,25 +11,70 @@ function BOTParentComponent() {
   return (
     <div>
       {!showWidget && (
-        <button
-          style={{
-            backgroundImage: theme
-              ? theme
-              : "linear-gradient(135deg, rgb(42, 39, 218) 0%, rgb(0, 204, 255) 100%)",
-            boxShadow: "rgba(0, 0, 0, 0.1) 0px 4px 12px",
-          }}
-          onClick={() => setShowWidget(!showWidget)}
-          className="fixed bottom-4 right-4 inline-flex items-center justify-center text-sm font-medium disabled:pointer-events-none disabled:opacity-50 rounded-full w-16 h-16 bg-black hover:bg-gray-700 m-0 cursor-pointer border-gray-200 bg-none p-0 normal-case leading-5 hover:text-gray-900"
-          type="button"
-          aria-haspopup="dialog"
-          aria-expanded="false"
-          data-state="closed"
-        >
-          <Icon
-            icon="fluent:chat-empty-24-regular"
-            className="w-9 h-9 text-white block border-gray-200 align-middle"
-          />
-        </button>
+        <div>
+          <span
+            className="fixed bottom-7 right-24 py-1 px-2 rounded-md text-nowrap whitespace-nowrap truncate text-[15px]"
+            style={{
+              color: "#3c4859",
+              boxShadow: "rgb(182, 190, 252) 0px 0px 13px",
+            }}
+          >
+            Hey there 👋
+          </span>
+          <button
+            style={{
+              backgroundImage: theme
+                ? theme
+                : "linear-gradient(135deg, rgb(42, 39, 218) 0%, rgb(0, 204, 255) 100%)",
+              boxShadow: "rgba(0, 0, 0, 0.1) 0px 4px 12px",
+            }}
+            onClick={() => setShowWidget(!showWidget)}
+            className="EMBOT-ripple-effect fixed bottom-4 right-4 inline-flex items-center justify-center text-sm font-medium disabled:pointer-events-none disabled:opacity-50 rounded-full w-16 h-16 bg-black hover:bg-gray-700 m-0 cursor-pointer border-gray-200 bg-none p-0 normal-case leading-5 hover:text-gray-900 ripple"
+            type="button"
+            aria-haspopup="dialog"
+            aria-expanded="false"
+            data-state="closed"
+          >
+            <Icon
+              icon="fluent:chat-empty-24-regular"
+              className="w-9 h-9 text-white block border-gray-200 align-middle"
+            />
+          </button>
+
+          <style jsx>{`
+            .EMBOT-ripple-effect {
+              animation-name: ripple !important;
+              animation-duration: 1.5s !important;
+              animation-iteration-count: infinite !important;
+              border-radius: 50% !important;
+            }
+            .EMBOT-ripple-effect .icon {
+              transform: rotate(-60deg);
+              animation: rotate 0.2s ease forwards;
+            }
+            @keyframes rotate {
+              from {
+                transform: rotate(-60deg);
+              }
+              to {
+                transform: rotate(0deg);
+              }
+            }
+            @keyframes ripple {
+              0% {
+                box-shadow: 0 0 0 0 green, 0 0 0 0 #fff4;
+              }
+
+              80% {
+                box-shadow: 0 0 0 20px #fff0, 0 0 0 40px #fff0;
+              }
+
+              100% {
+                box-shadow: 0 0 0 0 #fff0, 0 0 0 0 #fff0;
+              }
+            }
+          `}</style>
+        </div>
       )}
 
       {showWidget && <ChatBotWidget />}
--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -62,35 +62,66 @@ const ChatContainer = () => {
   }, [arrivalMsg]);
 
   return (
-    <div
-      id="EmChatBotChatContainerId"
-      ref={chatContainerRef}
-      style={{ height: "calc(100% - 160px)", maxHeight: "calc(100% - 160px)" }}
-      className="chat-container pr-4 p-6 overflow-y-auto flex-1 min-h-[167px] scroll-smooth focus:scroll-auto"
-    >
-      {chatMessages?.map((msgData, index) => (
-        <div key={index} className="w-full min-h-fit">
-          {msgData?.myself != false ? (
-            <>
-              <Response response={msgData} index={index} />
-            </>
-          ) : (
-            <UserTrigger userTrigger={msgData} index={index} />
-          )}
-          {msgData?.format == "slider" && (
-            <SliderTriggerComponent data={msgData} />
-          )}
-        </div>
-      ))}
+    <>
+      <div
+        id="EmChatBotChatContainerId"
+        ref={chatContainerRef}
+        style={{
+          height: "calc(100% - 160px)",
+          maxHeight: "calc(100% - 160px)",
+        }}
+        className="chat-container pr-4 p-6 overflow-y-auto flex-1 min-h-[167px] scroll-smooth focus:scroll-auto"
+      >
+        {chatMessages?.map((msgData, index) => (
+          <div key={index} className="w-full min-h-fit">
+            {msgData?.myself != false ? (
+              <>
+                <Response response={msgData} index={index} />
+              </>
+            ) : (
+              <UserTrigger userTrigger={msgData} index={index} />
+            )}
+            {msgData?.format == "slider" && (
+              <SliderTriggerComponent data={msgData} />
+            )}
+          </div>
+        ))}
 
-      {assitWaitingTimerData?.status && (
-        <CountdownTimer
-          initialMinutes={assitWaitingTimerData?.time?.min || 0}
-          initialSeconds={assitWaitingTimerData?.time?.sec || 15}
-          assitWaitingTimerData={assitWaitingTimerData}
-        />
-      )}
-    </div>
+        {assitWaitingTimerData?.status && (
+          <CountdownTimer
+            initialMinutes={assitWaitingTimerData?.time?.min || 0}
+            initialSeconds={assitWaitingTimerData?.time?.sec || 15}
+            assitWaitingTimerData={assitWaitingTimerData}
+          />
+        )}
+      </div>
+      <style jsx>{`
+        #EmChatBotChatContainerId::-webkit-scrollbar {
+          width: 0px;
+          height: 0px; 
+        }
+
+        #EmChatBotChatContainerId::-webkit-scrollbar-track {
+          background: transparent;
+        }
+
+        #EmChatBotChatContainerId::-webkit-scrollbar-thumb {
+          background-color: #888;
+          border-radius: 10px;
+          border: 2px solid #f1f1f1;
+        }
+
+        /* Hide scrollbar by default */
+        #EmChatBotChatContainerId::-webkit-scrollbar {
+          display: none;
+        }
+
+        /* Show scrollbar on hover */
+        #EmChatBotChatContainerId:hover::-webkit-scrollbar {
+          display: block;
+        }
+      `}</style>
+    </>
   );
 };
 
